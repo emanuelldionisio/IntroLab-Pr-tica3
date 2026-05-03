4969acb2f03e7116b6cf6899a582e382d32c13e9
emanuelldionisio <emanuel.eduardo@academico.ifpb.edu.br>
2026-05-03 11:35:24 -0300

    Adicionei o ângulo e o eixo x na plotagem da tarefa 2. Falta achar um ajuste melhor para o y.

--- Tarefa2/codigo/plotagem.py
+++ Tarefa2/codigo/plotagem.py
@@ -4,6 +4,8 @@ import scipy.optimize as opt
 from func_seno import func_seno
 import csv
 
+diretorioImgs = "Tarefa2/imgs/semDissipacao"
+
 data = []
 with open('Tarefa2/data/SemDissipacao.csv', 'r') as file:
     reader = csv.reader(file)
@@ -17,17 +19,61 @@ def plotagemPosx():
     tempo = data[:,0]
     posx = data[:,1]
     
-    popt, pcov = opt.curve_fit(func_seno, tempo, posx)
+    popt, pcov = opt.curve_fit(func_seno, tempo, posx, p0=[1, 3, 0, 0])
     posxAjustada = func_seno(tempo, *popt)
-        
+    erro = np.diag(pcov)**0.5
+    print(erro[1])    
+    plt.figure(figsize=(19.2, 10.8))
     plt.title("Posição em X em função do tempo")
     plt.xlabel("Tempo (s)")
     plt.ylabel("Posição em X (m)")
     plt.plot(tempo, posx, 'o', label='Dados experimentais', markersize=1)
-    plt.plot(tempo, posxAjustada, 'r-', label=f'Ajuste de curvas - g = {popt[1]**2:.2f} m/s²')
+    plt.plot(tempo, posxAjustada, 'r-', label=f'Ajuste de curvas - g = {popt[1]**2:.4f} m/s²')
     plt.legend()
-    plt.show()
+    plt.savefig(diretorioImgs + "/posx.png")
+    plt.close()
+
+def plotagemPosy():
+    tempo = data[:,0]
+    posy = data[:,2]
+    popt, pcov = opt.curve_fit(func_seno, tempo, posy, p0=[(np.max(posy)-np.min(posy))/2, 3, 0, 0.01])
+    posyAjustada = func_seno(tempo, *popt)
+    erro = np.diag(pcov)**0.5
+    print(erro[1])    
+    plt.figure(figsize=(19.2, 10.8))
+    plt.title("Posição em Y em função do tempo")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Posição em Y (m)")
+    plt.plot(tempo, posy, 'o', label='Dados experimentais', markersize=1)
+    plt.plot(tempo, posyAjustada, 'r-', label=f'Ajuste de curvas - g = {popt[1]**2:.4f} m/s²')
+    plt.legend()
+    plt.savefig(diretorioImgs + "/posy.png")
+    plt.close()
+
+def plotagemAng():
+    tempo = data[:,0]
+    posx = data[:,1]
+    posy = data[:,2]
+    y0 = np.min(posy)
+
+    ang = np.arctan2(posx, 1 - (posy - y0))
+    
+    popt, pcov = opt.curve_fit(func_seno, tempo, ang, p0=[(np.max(ang)-np.min(ang))/2, 3, 0, 0])
+    angAjustada = func_seno(tempo, *popt)
+    erro = np.diag(pcov)**0.5
+    print(erro[1])    
+    plt.figure(figsize=(19.2, 10.8))
+    plt.title("Ângulo em função do tempo")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Ângulo (rad)")
+    plt.plot(tempo, ang, 'o', label='Dados experimentais', markersize=1)
+    plt.plot(tempo, angAjustada, 'r-', label=f'Ajuste de curvas - g = {popt[1]**2:.4f} m/s²')
+    plt.legend()
+    plt.savefig(diretorioImgs + "/ang.png")
+    plt.close()
 
 if __name__ == "__main__":
     plotagemPosx()
+    plotagemPosy()
+    plotagemAng()
 
