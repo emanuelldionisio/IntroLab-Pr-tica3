2c78abf88be533d9ca827eab0e877a278e2b4d11
emanuelldionisio <emanuel.eduardo@academico.ifpb.edu.br>
2026-05-03 20:38:03 -0300

    a energia nao ta dando certo

--- Tarefa2/codigo/plotagem.py
+++ Tarefa2/codigo/plotagem.py
@@ -5,9 +5,10 @@ from func_seno import func_seno
 import csv
 
 diretorioImgs = "Tarefa2/imgs/semDissipacao"
+diretorioData = "Tarefa2/data/semDissipacao"
 
 data = []
-with open('Tarefa2/data/SemDissipacao.csv', 'r') as file:
+with open(diretorioData + '/SemDissipacao.csv', 'r') as file:
     reader = csv.reader(file)
     data = list(reader)
 #Cabeçalho: [Tempo, Posição em X, Posição em Y, Energia cinética]
@@ -15,75 +16,78 @@ for i in range(1, len(data)):
     data[i] = [i if i != '' else None for i in data[i]]
 data = np.array(data[1:], dtype=float)  
 
+tempo = data[:,0]
+posx = data[:,1]
+posy = data[:,2]
+energiaK = data[:,3]
+y0 = np.min(posy)
+ang = -np.arctan2(posx, 1 - (posy - y0))
+ang0 = ang[0]
+
 def plotagemPosx():
-    tempo = data[:,0]
-    posx = data[:,1]
     
     popt, pcov = opt.curve_fit(func_seno, tempo, posx, p0=[1, 3, 0, 0])
     posxAjustada = func_seno(tempo, *popt)
     erro = np.diag(pcov)**0.5
-    print(erro[1])    
-    plt.figure(figsize=(19.2, 10.8))
+    with open(diretorioData + "/ajustePosx.txt", 'w') as f:
+        f.write(f"Parâmetros do ajuste: {popt}\n")
+        f.write(f"Erro dos parâmetros: {erro}\n")
+     
     plt.title("Posição em X em função do tempo")
     plt.xlabel("Tempo (s)")
     plt.ylabel("Posição em X (m)")
     plt.plot(tempo, posx, 'o', label='Dados experimentais', markersize=1)
     plt.plot(tempo, posxAjustada, 'r-', label=f'Ajuste de curvas')
-    plt.legend()
+    plt.legend(loc='upper right')
     plt.savefig(diretorioImgs + "/posx.png")
     plt.close()
 
 def plotagemPosy():
-    tempo = data[:,0]
-    posy = data[:,2]
     popt, pcov = opt.curve_fit(func_seno, tempo, posy, p0=[0.013, 2 * np.pi, np.pi/2, 0.021])
     posyAjustada = func_seno(tempo, *popt)
     erro = np.diag(pcov)**0.5
-    print(erro[1])    
-    plt.figure(figsize=(19.2, 10.8))
+    with open(diretorioData + "/ajustePosy.txt", 'w') as f:
+        f.write(f"Parâmetros do ajuste: {popt}\n")
+        f.write(f"Erro dos parâmetros: {erro}\n")  
     plt.title("Posição em Y em função do tempo")
     plt.xlabel("Tempo (s)")
     plt.ylabel("Posição em Y (m)")
     plt.plot(tempo, posy, 'o', label='Dados experimentais', markersize=1)
     plt.plot(tempo, posyAjustada, 'r-', label=f'Ajuste de curvas')
-    plt.legend()
+    plt.legend(loc='upper right')
     plt.savefig(diretorioImgs + "/posy.png")
     plt.close()
 
 def plotagemAng():
-    tempo = data[:,0]
-    posx = data[:,1]
-    posy = data[:,2]
-    y0 = np.min(posy)
-
-    ang = np.arctan2(posx, 1 - (posy - y0))
     
     popt, pcov = opt.curve_fit(func_seno, tempo, ang, p0=[(np.max(ang)-np.min(ang))/2, 3, 0, 0])
     angAjustada = func_seno(tempo, *popt)
     erro = np.diag(pcov)**0.5
-    print(erro[1])    
-    plt.figure(figsize=(19.2, 10.8))
+    with open(diretorioData + "/ajusteAng.txt", 'w') as f:
+        f.write(f"Parâmetros do ajuste: {popt}\n")
+        f.write(f"Erro dos parâmetros: {erro}\n")
     plt.title("Ângulo em função do tempo")
     plt.xlabel("Tempo (s)")
     plt.ylabel("Ângulo (rad)")
     plt.plot(tempo, ang, 'o', label='Dados experimentais', markersize=1)
     plt.plot(tempo, angAjustada, 'r-', label=f'Ajuste de curvas - g = {popt[1]**2:.4f} m/s²')
-    plt.legend()
+    plt.legend(loc='upper right')
     plt.savefig(diretorioImgs + "/ang.png")
     plt.close()
 
 def plotagemEnergia():
-    tempo = data[:,0]
-    posy = data[:,2]
-    posx = data[:,1]
+    
+    m = 0.0518
     vx = np.gradient(posx, tempo)
     vy = np.gradient(posy, tempo)
-    v = np.sqrt(vx**2 + vy**2)
-    energiaK = 0.5 * 0.0518 * v**2
-    energiaPot = 9.8 * 0.0518 * posy
+    ax = np.gradient(vx, tempo)
+    ay = np.gradient(vy, tempo)
+    
+    
+    energiaPot = 9.8 * (posy - y0) * m
     energiaMec = energiaK + energiaPot
     
-    plt.figure(figsize=(19.2, 10.8))
+    plt.figure(figsize=(10, 6))
     plt.title("Energia em função do tempo")
     plt.xlabel("Tempo (s)")
     plt.ylabel("Energia (J)")
@@ -91,7 +95,7 @@ def plotagemEnergia():
     plt.plot(tempo, energiaK, 'o', label='Dados experimentais - Energia Cinética', markersize=1)
     plt.plot(tempo, energiaMec, 'o', label='Dados experimentais - Energia Mecânica', markersize=1)
     
-    plt.legend()
+    plt.legend(loc='upper right')
     plt.savefig(diretorioImgs + "/energia.png")
     plt.close()
 
