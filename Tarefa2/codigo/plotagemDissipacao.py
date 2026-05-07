eec654b25e80bb562de468f111b7f0a0fbf1145f
Emanuel Eduardo Oliveira Dionisio <emanuel.eduardo@academico.ifpb.edu.br>
2026-05-07 20:25:48 +0000

    Fiz o gráfico da dissipação na tarefa do tracker

--- Tarefa2/codigo/plotagemDissipacao.py
+++ Tarefa2/codigo/plotagemDissipacao.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
 from func_seno_amortecido import func_seno_amortecido
+from func_seno import func_seno
 import csv
 
 diretorioImgs = "Tarefa2/imgs/comDissipacao"
@@ -22,6 +23,27 @@ posy = data[:,2]
 energiaK = data[:,3] * 0.0518
 y0 = np.min(posy)
 posy = posy - y0
+ang = -np.arctan2(posx, 1.107 - (posy - y0))
+ang0 = ang[0]
+
+def plotagemAng():
+    
+    popt, pcov = opt.curve_fit(func_seno_amortecido, tempo, ang, p0=[1, 1, 3, 0.1, 0.1])
+    semDissipacao = func_seno_amortecido(tempo, 0, popt[1], popt[2], popt[3], popt[4])
+    angAjustada = func_seno_amortecido(tempo, *popt)
+    erro = np.diag(pcov)**0.5
+    with open(diretorioData + "/ajusteAng.txt", 'w') as f:
+        f.write(f"Parâmetros do ajuste: {popt}\n")
+        f.write(f"Erro dos parâmetros: {erro}\n")
+    plt.title("Ângulo em função do tempo")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Ângulo (rad)")
+    plt.plot(tempo, ang, 'o', label='Dados experimentais', markersize=1)
+    plt.plot(tempo, angAjustada, '--', color="red", label=f'Ajuste de curvas')
+    plt.plot(tempo, semDissipacao, '-.', color="green", label=f'Ajuste sem dissipação')
+    plt.legend(loc='upper right')
+    plt.savefig(diretorioImgs + "/ang.png")
+    plt.close()
 
 def plotagemPosx():
     
@@ -42,17 +64,10 @@ def plotagemPosx():
     plt.close()
 
 def plotagemPosy():
-    popt, pcov = opt.curve_fit(func_seno_amortecido, tempo, posy, p0=[1, .5, 6.5, 0.0001, 0])
-    posyAjustada = func_seno_amortecido(tempo, *popt)
-    erro = np.diag(pcov)**0.5
-    with open(diretorioData + "/ajustePosy.txt", 'w') as f:
-        f.write(f"Parâmetros do ajuste: {popt}\n")
-        f.write(f"Erro dos parâmetros: {erro}\n")  
     plt.title("Posição em Y em função do tempo")
     plt.xlabel("Tempo (s)")
     plt.ylabel("Posição em Y (m)")
     plt.plot(tempo, posy, 'o', label='Dados experimentais', markersize=1)
-    plt.plot(tempo, posyAjustada, 'r-', label=f'Ajuste de curvas')
     plt.legend(loc='upper right')
     plt.savefig(diretorioImgs + "/posy.png")
     plt.close()
@@ -78,6 +93,7 @@ def plotagemEnergia():
     plt.close()
 
 if __name__ == "__main__":
+    plotagemAng()
     plotagemPosx()
     plotagemPosy()
     plotagemEnergia()
